618f8c7cc841ec303a8c373111e7de5ff4ab20e6
v-moshe-p <moshe@vcita.com>
2021-01-05 12:08:27 +0200

    remove key

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -49,7 +49,7 @@ export default {
 
 //https://developers.themoviedb.org/3/getting-started/introduction
 // const query = "Jurassic Park";
-// const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
+// const url = `https://api.themoviedb.org/3/search/movie?api_key=&language=en-US&query=${query}&page=1&include_adult=false`;
 // const url = 'movie/550?api_key='
 
 // /movie/top_rated
